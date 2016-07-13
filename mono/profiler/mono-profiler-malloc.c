46bb8af69e43c0bdeb23244d5530f1696053f5e5
Rodrigo Kumpera <kumpera@gmail.com>
2016-07-13 14:56:43 -0700

    precise alloc tracking.

--- mono/profiler/mono-profiler-malloc.c
+++ mono/profiler/mono-profiler-malloc.c
@@ -23,24 +23,27 @@
 #include <stdlib.h>
 #include <glib.h>
 #include <sys/stat.h>
+#include <pthread.h>
 
 //OSX only
 #include <malloc/malloc.h>
 
+//XXX for now all configuration is here
+#define PRINT_ALLOCATION FALSE
+#define PRINT_MEMDOM FALSE
+#define PRINT_RT_ALLOC FALSE
 
 void mono_profiler_startup (const char *desc);
-
+static void dump_alloc_stats (void);
 
 struct _MonoProfiler {
 	int filling;
 };
 
-static size_t alloc_bytes, alloc_count, malloc_waste, realloc_waste;
 
 static void
 prof_shutdown (MonoProfiler *prof)
 {
-	printf ("profiling done\n");
 }
 
 static const char *memdom_name[] = {
@@ -50,60 +53,89 @@ static const char *memdom_name[] = {
 	"image-set",
 };
 
-static int last_time;
-static void
-dump_alloc (void)
-{
-	++last_time;
-	if (last_time % 100)
-		return;
-	printf ("alloc %zu alloc count %zu malloc waste %zu realloc waste %zu\n",
-		alloc_bytes, alloc_count, malloc_waste, realloc_waste);
-}
 
 static void
 memdom_new (MonoProfiler *prof, void* memdom, MonoProfilerMemoryDomain kind)
 {
-	printf ("memdom new %p type %s\n", memdom, memdom_name [kind]);
-	dump_alloc ();
+	if (PRINT_MEMDOM)
+		printf ("memdom new %p type %s\n", memdom, memdom_name [kind]);
+	dump_alloc_stats ();
 }
 
 static void
 memdom_destroy (MonoProfiler *prof, void* memdom)
 {
-	printf ("memdom destroy %p\n", memdom);
-	dump_alloc ();
+	if (PRINT_MEMDOM)
+		printf ("memdom destroy %p\n", memdom);
+	dump_alloc_stats ();
 }
 
 static void
 memdom_alloc (MonoProfiler *prof, void* memdom, size_t size, const char *tag)
 {
-	printf ("memdom %p alloc %zu %s\n", memdom, size, tag);
+	if (PRINT_RT_ALLOC)
+		printf ("memdom %p alloc %zu %s\n", memdom, size, tag);
 }
 
 static void
 runtime_malloc_event (MonoProfiler *prof, void *address, size_t size, const char *tag)
 {
-	printf ("malloc %p %zu %s\n", address, size, tag);
+	if (PRINT_RT_ALLOC)
+		printf ("malloc %p %zu %s\n", address, size, tag);
 }
 
 static void
 runtime_free_event (MonoProfiler *prof, void *address, size_t size, const char *tag)
 {
-	printf ("free %p %zu %s\n", address, size, tag);
+	if (PRINT_RT_ALLOC)
+		printf ("free %p %zu %s\n", address, size, tag);
 }
 
 static void
 runtime_valloc_event (MonoProfiler *prof, void *address, size_t size, const char *tag)
 {
-	printf ("valloc %p %zu %s\n", address, size, tag);
+	if (PRINT_RT_ALLOC)
+		printf ("valloc %p %zu %s\n", address, size, tag);
 }
 
 static void
 runtime_vfree_event (MonoProfiler *prof, void *address, size_t size, const char *tag)
 {
-	printf ("vfree %p %zu %s\n", address, size, tag);
+	if (PRINT_RT_ALLOC)
+		printf ("vfree %p %zu %s\n", address, size, tag);
 }
+
+
+typedef struct _AllocInfo AllocInfo;
+
+/* malloc tracking */
+struct _AllocInfo {
+	void *address;
+	size_t size;
+	size_t waste;
+	AllocInfo *next;
+};
+
+static pthread_mutex_t alloc_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void
+alloc_lock (void)
+{
+	pthread_mutex_lock (&alloc_mutex);
+}
+
+static void
+alloc_unlock (void)
+{
+	pthread_mutex_unlock (&alloc_mutex);
+}
+
+/* maybe we could make it dynamic later */
+#define TABLE_SIZE 1019
+static AllocInfo *alloc_table [TABLE_SIZE];
+
+static size_t alloc_bytes, alloc_count, alloc_waste;
+
 
 static void __attribute__((noinline))
 break_on_malloc_waste (void)
@@ -111,51 +143,116 @@ break_on_malloc_waste (void)
 	
 }
 
+static size_t
+hash_ptr (size_t ptr)
+{
+	return (int)((ptr * 1737350767) ^ ((ptr * 196613) >> 16));
+}
+
+static void
+del_alloc (void *address)
+{
+	size_t addr = (size_t)address;
+	int bucket = hash_ptr (addr) % TABLE_SIZE;
+
+	AllocInfo **prev;
+
+	alloc_lock ();
+	for (prev = &alloc_table [bucket]; *prev; prev = &(*prev)->next) {
+		AllocInfo *info = *prev;
+		if (info->address == address) {
+			alloc_bytes -= info->size;
+			--alloc_count;
+			alloc_waste -= info->waste;
+
+			*prev = info->next;
+			g_free (info);
+			break;
+		}
+	}
+	alloc_unlock ();
+}
+
+
+static void
+add_alloc (void *address, size_t size)
+{
+	size_t addr = (size_t)address;
+	int bucket = hash_ptr (addr) % TABLE_SIZE;
+
+	AllocInfo *info = malloc (sizeof (AllocInfo));
+	info->address = address;
+	info->size = size;
+	info->waste = malloc_size (address) - size;
+
+	alloc_lock ();
+	info->next = alloc_table [bucket];
+	alloc_table [bucket] = info;
+
+	alloc_bytes += size;
+	++alloc_count;
+	alloc_waste += info->waste;
+
+	alloc_unlock ();
+
+	if (info->waste > 10)
+		break_on_malloc_waste ();
+
+}
+
+static void
+dump_alloc_stats (void)
+{
+	static int last_time;
+
+	if (!PRINT_ALLOCATION)
+		return;
+	++last_time;
+	if (last_time % 100)
+		return;
+	printf ("alloc %zu alloc count %zu waste %zu\n",
+		alloc_bytes, alloc_count, alloc_waste);
+}
+
+
 static void *
 platform_malloc (size_t size)
 {
-	__sync_fetch_and_add (&alloc_bytes, malloc_good_size (size));
-	__sync_fetch_and_add (&alloc_count, 1);
-	__sync_fetch_and_add (&malloc_waste, (malloc_good_size (size) - size));
-	dump_alloc ();
+	void * res = malloc (size);
+	add_alloc (res, size);
 
-	size_t waste = (malloc_good_size (size) - size);
-	if (waste > 10)
-		break_on_malloc_waste ();
-
-	return malloc (size);
+	dump_alloc_stats ();
+	return res;
 }
 
 static void *
 platform_realloc (void *mem, size_t count)
 {
-	size_t extra = malloc_good_size (count) - malloc_size (mem);
+	del_alloc (mem);
+	void * res = realloc (mem, count);
+	add_alloc (res, count);
 
-	__sync_fetch_and_add (&alloc_bytes, extra);
-	__sync_fetch_and_add (&alloc_count, 1);
-	__sync_fetch_and_add (&realloc_waste, (malloc_good_size (count) - count));
-	dump_alloc ();
-	 return realloc (mem, count);
+	dump_alloc_stats ();
+	return res;
  }
 
 static void
 platform_free (void *mem)
 {
-	__sync_fetch_and_add (&alloc_count, -1);
-	__sync_fetch_and_add (&alloc_bytes, -malloc_size (mem));
-	dump_alloc ();	
+	del_alloc (mem);
+	free (mem);
+
+	dump_alloc_stats ();
 }
 
 static void*
 platform_calloc (size_t count, size_t size)
 {
-	size_t actual = count * size;
-	__sync_fetch_and_add (&alloc_bytes, malloc_good_size (actual));
-	__sync_fetch_and_add (&alloc_count, 1);
-	__sync_fetch_and_add (&malloc_waste, (malloc_good_size (actual) - actual));
+	void * res = calloc (count, size);
+	add_alloc (res, count * size);
 
-	dump_alloc ();
-	 return calloc (count, size);
+	dump_alloc_stats ();
+	return res;
 }
 
 
