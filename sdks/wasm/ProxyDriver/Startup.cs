e64043f50aaa381b6c77c00caef02123796fe146
Rodrigo Kumpera <kumpera@gmail.com>
2018-10-05 18:20:35 -0400

    blarg

--- sdks/wasm/ProxyDriver/Startup.cs
+++ sdks/wasm/ProxyDriver/Startup.cs
@@ -9,6 +9,9 @@ using System.Threading;
 using System.IO;
 using System.Text;
 
+using  Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+
 namespace WsProxy
 {
 
@@ -49,14 +52,65 @@ namespace WsProxy
 			return new Uri ("ws://localhost:9222" + path);
 		}
 
+		async Task SendNodeVersion (HttpContext context) {
+			Console.WriteLine ("hello chrome! json/version");
+			var resp_obj = new JObject();
+			resp_obj["Browser"] = "node.js/v9.11.1";
+			resp_obj["Protocol-Version"] = "1.1";
+
+			var response = resp_obj.ToString ();
+			await context.Response.WriteAsync (response, new CancellationTokenSource ().Token);
+		}
+		async Task Halp (HttpContext context) {
+			Console.WriteLine ("Halp! ");
+			await context.Response.WriteAsync ("SOMETHING", new CancellationTokenSource ().Token);
+		}
+
+		async Task SendNodeList (HttpContext context) {
+			Console.WriteLine ("hello chrome! json/list");
+			try {
+			var response = new JArray (JObject.FromObject (new {
+				description = "node.js instance",
+				devtoolsFrontendUrl = "chrome-devtools://devtools/bundled/inspector.html?experiments=true&v8only=true&ws=localhost:9300/91d87807-8a81-4f49-878c-a5604103b0a4",
+				faviconUrl = "https://nodejs.org/static/favicon.ico",
+				id =  "91d87807-8a81-4f49-878c-a5604103b0a4",
+				title = "foo.js",
+				type = "node",
+				url = "file:///Users/kumpera/src/mono/sdks/wasm/dbg/foo.js",
+				webSocketDebuggerUrl = "ws://localhost:9300/91d87807-8a81-4f49-878c-a5604103b0a4"
+			})).ToString ();
+
+			Console.WriteLine ($"sending: {response}");
+			await context.Response.WriteAsync (response, new CancellationTokenSource ().Token);
+			} catch (Exception e) {Console.WriteLine(e);}
+		}
+
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure (IApplicationBuilder app, IHostingEnvironment env)
 		{
-			//loggerFactory.AddConsole();
-			//loggerFactory.AddDebug();
 			app.UseDeveloperExceptionPage ();
 
-			app.UseWebSockets (); app.UseRouter (router => {
+			app.UseWebSockets ();
+			
+			// var trackPackageRouteHandler = new RouteHandler(context =>
+			// {
+			// 	Console.WriteLine ("---trackPackageRouteHandler");
+			// 	var routeValues = context.GetRouteData().Values;
+			//
+			// 	Console.WriteLine($"aqui {string.Join(", ", routeValues)}");
+			//     return context.Response.WriteAsync(
+			//         $"Hello! Route values: {string.Join(", ", routeValues)}");
+			// });
+			//
+			// var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
+			// routeBuilder.MapGet ("json", SendNodeList);
+			// routeBuilder.MapGet ("json/list", SendNodeList);
+			// routeBuilder.MapGet ("json/version", SendNodeVersion);
+			// routeBuilder.MapGet ("91d87807-8a81-4f49-878c-a5604103b0a4", Halp);
+			//
+			// var routes = routeBuilder.Build();
+			// app.UseRouter(routes);
+			app.UseRouter (router => {
 				router.MapGet ("devtools/page/{pageId}", async context => {
 					if (!context.WebSockets.IsWebSocketRequest) {
 						context.Response.StatusCode = 400;
@@ -66,6 +120,27 @@ namespace WsProxy
 					try {
 						var proxy = new MonoProxy ();
 						var browserUri = GetBrowserUri (context.Request.Path.ToString ());
+						var ideSocket = await context.WebSockets.AcceptWebSocketAsync ();
+
+						await proxy.Run (browserUri, ideSocket);
+					} catch (Exception e) {
+						Console.WriteLine ("got exception {0}", e);
+					}
+				});
+				//Inspector API for using chrome devtools directly
+				router.MapGet ("json", SendNodeList);
+				router.MapGet ("json/list", SendNodeList);
+				router.MapGet ("json/version", SendNodeVersion);
+				router.MapGet ("91d87807-8a81-4f49-878c-a5604103b0a4", async context => {
+					if (!context.WebSockets.IsWebSocketRequest) {
+						context.Response.StatusCode = 400;
+						return;
+					}
+
+					try {
+						var proxy = new MonoProxy ();
+						// var browserUri = GetBrowserUri (context.Request.Path.ToString ());
+						var browserUri = new Uri ("ws://127.0.0.1:9229/5396589f-0696-4572-8027-0d4dacdb083c");
 						var ideSocket = await context.WebSockets.AcceptWebSocketAsync ();
 
 						await proxy.Run (browserUri, ideSocket);
